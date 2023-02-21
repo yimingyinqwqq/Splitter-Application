403273833fbd93060219a15a7b023fc05bbbd504
Ruozhen <ruozhen2@illinois.edu>
2023-02-20 18:29:21 -0600

    add login js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,29 @@
-// import React, { Component } from 'react';
-// import Navbar from './components/Navbar'
-// import Navbar from './NavBar'
-// import { Route, BrowserRouter, Routes } from 'react-router-dom'
-// import Home from './components/Home'
-// import About from './components/About'
-// import Schedule from './components/Schedule'
+import React, { Component } from 'react';
+import "./App.css"
+import "./Login.js"
 
-// import LoginPage from './components/LoginPage.tsx';
-// import AuthenticationPage from './components/pages/AuthenticationPage.tsx'
-// import RegisterPage from './components/pages/RegisterPage.tsx'
-// import NotFoundPage from './components/pages/NotFound.tsx'
+class App extends Component {
+  render() {
+    return (
+      <main id="main-holder">
+        
+        <h1 id="login-header">Login</h1>
 
-// /* "npm start" in "package.json" changed according to https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported */
+        <div id="login-error-msg-holder">
+          <p id="login-error-msg">Invalid username <span id="error-msg-second-line">and/or password</span></p>
+        </div>
 
-// import "./App.css"
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className='background'>
-//       <BrowserRouter>
-//         <div className="App">
-          
-//         </div>
-//       </BrowserRouter>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-
-// When the login button is clicked, the following code is executed
-if(loginButton){
-  loginButton.addEventListener("click", (e) => {
-    // Prevent the default submission of the form
-    e.preventDefault();
-    // Get the values input by the user in the form fields
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "user" && password === "web_dev") {
-        // If the credentials are valid, show an alert box and reload the page
-        alert("You have successfully logged in.");
-        window.location.reload();
-    } else {
-        // Otherwise, make the login error message show (change its oppacity)
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+        <form id="login-form">
+          <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username"/>
+          <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password"/>
+          <input type="submit" value="Login" id="login-form-submit"/>
+        </form>
+        
+      </main>
+      
+    );
+  }
 }
+
+ export default App;
+
