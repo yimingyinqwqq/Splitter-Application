6e15185d99f9ecbd10d93b041e3692167cd2b67d
yimingyinqwqq <78126725+yimingyinqwqq@users.noreply.github.com>
2023-02-20 21:24:13 -0600

    delete irrelavant files && add comments and refs

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,15 +1,18 @@
+// followed a simple tutorial https://medium.com/swlh/how-to-create-your-first-login-page-with-html-css-and-javascript-602dd71144f1
 import React, { useState } from 'react'
 
 const Login = () => {
     const [name, setName] = useState('');
     const [pwd, setPwd] = useState('');
 
+    // handle the onclick event for the Submit button
     const handleSubmit = (e) => {
         e.preventDefault();
         const username = name;
         const password = pwd;
         const loginErrorMsg = document.getElementById("login-error-msg");
 
+        // only for testing purposes
         if (username === "123" && password === "123") {
             alert("You have successfully logged in.");
             window.location.reload();
@@ -24,10 +27,12 @@ const Login = () => {
         
         <h1 id="login-header"> Login </h1>
 
+        {/* show error messages */}
         <div id="login-error-msg-holder">
           <p id="login-error-msg"> Invalid username <span id="error-msg-second-line"> and/or password </span></p>
         </div>
 
+        {/* form for authentication/login purposes */}
         <form id="login-form" onSubmit={handleSubmit}>
           <input type="text" name="username" id="username-field" class="login-form-field" placeholder="Username" 
             onChange = {(e) => setName(e.target.value)} value = {name}/>
@@ -35,8 +40,7 @@ const Login = () => {
           <input type="password" name="password" id="password-field" class="login-form-field" placeholder="Password"
             onChange = {(e) => setPwd(e.target.value)} value = {pwd}/>
 
-          {/* <input type="submit" value="Login" id="login-submit"/> */}
-          <button type="submit">Login</button>
+          <button type="submit"> Login </button>
         </form>
         
     </main>
