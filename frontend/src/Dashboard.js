fa514df5d991869cf27464623673378c2d85695a
yimingyinqwqq <78126725+yimingyinqwqq@users.noreply.github.com>
2023-02-27 21:34:18 -0600

    finish dashboard

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,12 +1,32 @@
-import React, { useEffect, useState } from "react";
+// reference from https://www.makeuseof.com/redirect-user-after-login-react/
+import React, { useEffect, useState, useRef } from "react";
 import { Navigate } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <p>Welcome to your Dashboard</p>
-        </div>
-    );
+    const authenticated = useRef(false);
+    authenticated.current = (sessionStorage.getItem("authenticated") === "true");
+
+    useEffect(() => {
+        const loggedInUser = sessionStorage.getItem("authenticated");
+
+        if (loggedInUser === true) {
+            authenticated.current = true;
+        }
+        
+    }, []);
+    
+
+    // render page according to value of "authenticated"
+    if (!authenticated.current) {
+        console.log(authenticated);
+        return <Navigate replace to="/login" />;
+    } else {
+        return (
+            <div>
+                <p> Welcome to your Dashboard </p>
+            </div>
+        );
+    }
 };
 
 export default Dashboard;
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -28,6 +28,9 @@ const Login = () => {
             // only for testing purposes
             if (username === "123" && password === "123") {
                 alert("You have successfully logged in.");
+                
+                // set sessionStorage for authenticating purposes
+                sessionStorage.setItem("authenticated", true);
                 navigate('/dashboard', { replace: true });
             } else {
                 loginErrorMsg.style.opacity = 1;
@@ -44,28 +47,28 @@ const Login = () => {
 
 
     return (
-    <main id="main-holder">
-        
-        <h1 id="login-header"> Login </h1>
+        <main id="main-holder">
+            
+            <h1 id="login-header"> Login </h1>
 
-        {/* show error messages */}
-        <div id="login-error-msg-holder">
-          <p id="login-error-msg"> Invalid username <span id="error-msg-second-line"> and/or password </span></p>
-        </div>
+            {/* show error messages */}
+            <div id="login-error-msg-holder">
+            <p id="login-error-msg"> Invalid username <span id="error-msg-second-line"> and/or password </span></p>
+            </div>
 
-        {/* form for authentication/login purposes */}
-        <form id="login-form" onSubmit={handleSubmit}>
-          <input type="text" name="username" id="username-field" className="login-form-field" placeholder="Username" 
-            onChange = {(e) => setName(e.target.value)} value = {name}/>
+            {/* form for authentication/login purposes */}
+            <form id="login-form" onSubmit={handleSubmit}>
+            <input type="text" name="username" id="username-field" className="login-form-field" placeholder="Username" 
+                onChange = {(e) => setName(e.target.value)} value = {name}/>
 
-          <input type="password" name="password" id="password-field" className="login-form-field" placeholder="Password"
-            onChange = {(e) => setPwd(e.target.value)} value = {pwd}/>
+            <input type="password" name="password" id="password-field" className="login-form-field" placeholder="Password"
+                onChange = {(e) => setPwd(e.target.value)} value = {pwd}/>
 
-          <button onClick={() => (state.button = 1)} type="submit" name="btn1" value="btn1"> Login </button>
-          <button onClick={() => (state.button = 2)} type="submit" name="btn2" value="btn2"> Google Login </button>
-        </form>
-        
-    </main>
+            <button onClick={() => (state.button = 1)} type="submit" name="btn1" value="btn1"> Login </button>
+            <button onClick={() => (state.button = 2)} type="submit" name="btn2" value="btn2"> Google Login </button>
+            </form>
+            
+        </main>
     )
 }
 
